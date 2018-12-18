10995d9789be2b57517bdf62f500808f2a3d0fb9
Tom Klaver <tomklav@gmail.com>
2018-12-18 16:37:04 +0100

    set nginx ingress svc kind to NodePort

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 kubectl apply -f rbac-tiller.yaml
 helm init --service-account tiller
-helm install stable/nginx-ingress --namespace kube-system --set controller.hostNetwork=true,controller.kind=DaemonSet
+helm install stable/nginx-ingress --namespace kube-system --set controller.hostNetwork=true,controller.kind=DaemonSet,controller.service.type=NodePort
 # add firewall rules (80/443)
 helm install --name cert-manager --namespace kube-system stable/cert-manager --set createCustomResource=false
 helm upgrade --install --namespace kube-system cert-manager stable/cert-manager --set createCustomResource=true
