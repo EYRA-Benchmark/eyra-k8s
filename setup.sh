f2e4dbeb6fc7ff38a0a9f8a606c25121139c5e0f
Tom Klaver <tomklav@gmail.com>
2018-12-18 17:00:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 kubectl apply -f rbac-tiller.yaml
 helm init --service-account tiller
-helm install stable/nginx-ingress --namespace kube-system --set controller.hostNetwork=true,controller.kind=DaemonSet,controller.service.type=NodePort
+helm install stable/nginx-ingress --namespace kube-system --set controller.hostNetwork=true,controller.kind=DaemonSet,controller.service.type=ClusterIP
 # add firewall rules (80/443)
 helm install --name cert-manager --namespace kube-system stable/cert-manager --set createCustomResource=false
 helm upgrade --install --namespace kube-system cert-manager stable/cert-manager --set createCustomResource=true
